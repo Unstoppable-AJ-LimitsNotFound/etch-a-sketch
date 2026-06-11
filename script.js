19aedb4b71aa05f007513703efb28806d411f0c7
Arav Jain <240245776+Unstoppable-AJ-LimitsNotFound@users.noreply.github.com>
2026-06-11 10:26:04 +0000

    Style grid layout

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const gridDiv = document.createElement("div");
     gridDiv.classList.add("gridDiv");
 
