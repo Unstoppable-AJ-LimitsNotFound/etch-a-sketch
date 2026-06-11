e2e65cad727646c1f990028af7b73306d29fa861
Arav Jain <240245776+Unstoppable-AJ-LimitsNotFound@users.noreply.github.com>
2026-06-11 17:40:49 +0000

    Implement grid resize feature

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-const container = document.querySelector("#container");
+const body = document.querySelector("body");
+let container = document.querySelector("#container");
 
-for (let i = 0; i < 256; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.classList.add("gridDiv");
 
-    container.appendChild(gridDiv);
+function makeGrid(numOfCellsInRow) {
+
+    let totalNumOfCellsInGrid = numOfCellsInRow * numOfCellsInRow;
+
+    for (let i = 0; i < totalNumOfCellsInGrid; i++) {
+        const gridDiv = document.createElement("div");
+        gridDiv.classList.add("gridDiv");
+        let sideLengthOfCell = (400 / numOfCellsInRow);
+        gridDiv.style.height = sideLengthOfCell + "px";
+        gridDiv.style.width = sideLengthOfCell + "px";
+        
+        
+        container.appendChild(gridDiv);
+    }
 }
 
-const gridDivs = document.querySelectorAll(".gridDiv");
-gridDivs.forEach((gridDiv) => {
-    gridDiv.addEventListener("mouseenter", () => {
-        gridDiv.classList.add("hovered");
+function colorGrid() {
+    const gridDivs = document.querySelectorAll(".gridDiv");
+    gridDivs.forEach((gridDiv) => {
+        gridDiv.addEventListener("mouseenter", () => {
+            gridDiv.classList.add("hovered");
+        });
     });
+}
+
+function removeGrid() {
+    container.remove();
+    container = document.createElement("div");
+    container.id = "container";
+
+    body.appendChild(container);
+}
+
+
+//Execution flow
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    numOfCellsInRow = Number(prompt("Enter the number of cells you want in a row or column of the grid (<100): "));
+    
+    removeGrid();
+    
+    makeGrid(numOfCellsInRow);
+
+    colorGrid();
+
 });
+
